da8310db4831d48301f7db32198e05c12b1aa68b
Yagyansh Gupta <147188891+Qureshi02@users.noreply.github.com>
2025-08-26 20:51:24 +0530

    add user registration functionality and user model

---
+++ src/models/user.models.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    fullname: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      minlength: [2, "Full name must be at least 2 characters"],
+      maxlength: [100, "Full name too long"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [4, "Password must be at least 4 characters long"],
+    },
+    role: {
+      type: String,
+      enum: ["patient", "therapist", "supervisor"],
+      default: "patient",
+    },
+    refreshtoken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export const User = mongoose.model("User", userSchema);
